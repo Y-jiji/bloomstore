e41bc283cb46d3b200ca0b806a14bb506ef39fe0
y-jiji <y.jijiji.data.science@gmail.com>
2024-02-12 15:41:26 +0800

    Feature: partitioner.

--- include/partitioner.hpp
+++ include/partitioner.hpp
@@ -9,7 +9,10 @@ class Partitioner {
     std::vector<BloomStore> instances;
 
     public:
-    Partitioner(std::vector<BloomStore>);
+    Partitioner(std::vector<BloomStore> instances);
+    void Put(std::span<uint8_t> key, std::span<uint8_t> value);
+    void Del(std::span<uint8_t> key);
+    void Get(std::span<uint8_t> key, std::span<uint8_t> value, bool& is_tombstone, bool& is_found);
 
 };
 
--- lib/partitioner.cpp
+++ lib/partitioner.cpp
@@ -4,6 +4,25 @@
 
 namespace bloomstore {
 
+Partitioner::Partitioner(
+    std::vector<BloomStore> instances
+):
+    instances{instances}
+{}
 
+void Partitioner::Put(std::span<uint8_t> key, std::span<uint8_t> value) {
+    size_t index = Hash(key, 'Z') % this->instances.size();
+    this->instances[index].Put(key, value);
+}
+
+void Partitioner::Del(std::span<uint8_t> key) {
+    size_t index = Hash(key, 'Z') % this->instances.size();
+    this->instances[index].Del(key);
+}
+
+void Partitioner::Get(std::span<uint8_t> key, std::span<uint8_t> value, bool& is_tombstone, bool& is_found) {
+    size_t index = Hash(key, 'Z') % this->instances.size();
+    this->instances[index].Get(key, value, is_tombstone, is_found);
+}
 
 } // namespace bloomstore
